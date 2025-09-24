d6ef445c10fbdd72035923c6598397a955164d76
Jim Howard <ec2-user@ip-172-31-22-235.ec2.internal>
2025-09-24 21:28:07 +0000

    upload

---
+++ test_upload.py
@@ -0,0 +1,18 @@
+import boto3
+
+def upload_file():
+    bucket_name = "my-example-bucket"   # change me
+    file_name = "local_test.txt"
+    object_name = "teaching_demo/hello.txt"
+    
+    # Write a simple file
+    with open(file_name, "w") as f:
+        f.write("Hello from test_upload.py!\n")
+    
+    s3 = boto3.client("s3")
+    s3.upload_file(file_name, bucket_name, object_name)
+    
+    print(f"✅ Uploaded {file_name} to s3://{bucket_name}/{object_name}")
+
+if __name__ == "__main__":
+    upload_file()
